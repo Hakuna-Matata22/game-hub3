58ac087c5e021679f1a453ed04b90b49244a0518
Leo <leolemuel.federico@gmail.comgit config --global user.email leolemuel.federico@gmail.com>
2023-04-09 09:35:55 +0800

    Refactor removed duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,9 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
-import React from "react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+
 import { Games } from "../hooks/useGames";
+import getCroppedImageUrl from "../Services/image-url";
+import MetaCriticScore from "./MetaCriticScore";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Games;
@@ -8,10 +11,16 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+    <Card>
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <MetaCriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,15 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames, { Games } from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 function GameGrid() {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22,
+  ];
 
   return (
     <>
@@ -13,8 +19,16 @@ function GameGrid() {
         padding="10px"
         spacing={10}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
